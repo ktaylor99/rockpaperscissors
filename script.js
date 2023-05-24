42becf940833649709c6cba43500b84abebdf14b
ktaylor99 <132228170+ktaylor99@users.noreply.github.com>
2023-05-24 17:58:24 -0500

    Created function for computer's chochoice

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-/* 
 function getComputerChoice(){
-    variables of rock, paper, scissors with values of 1, 2, and 3
-    something to pick one of those from random
+    // choices for computer
+   var choices = ["rock", "paper", "scissors"]
+   // defining random choice
+   var random = Math.floor((Math.random() * choices.length))
+   // creating random choice
+   var randomChoice=choices[random];
+   console.log(randomChoice);
 }
-
-function playRound(player selection, computer selection){
-    this just pulls up what the player and computer picked 
-    and puts them against each other i think i actually
-    have no clue
-    if statement about what beats what
-    return string about that 
-
-}
-define somewhere what beats what, i.e if rock is 1 and scissors is 3, 1 > 3 or something??
-and add some sort of loop to do five rounds 
+getComputerChoice();
