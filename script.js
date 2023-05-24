3e50a6d46875fa1b6a4549386e004ed56098eda2
ktaylor99 <132228170+ktaylor99@users.noreply.github.com>
2023-05-24 16:01:52 -0500

    Scrapped code, rewriting

--- script.js
+++
@@ -1,8 +0,0 @@
-function getComputerChoice(){
-    var array = ["Rock", "Paper", "Scissors"]
-    var index = Math.floor(Math.random() * array.length);
-    var chosenChoice = array[index];
-
-    console.log(chosenChoice)
-}
-getComputerChoice();
